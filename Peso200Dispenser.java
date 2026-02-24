734d6fc8ec3f7a1ce213db08057a88f4ce4f6fc2
tomacruzjosh <tomacruzj415@gmail.com>
2026-02-24 08:45:13 +0800

    Create Peso200Dispenser.java
    This is a modification on the original program. :)

---
+++ Peso200Dispenser.java
@@ -0,0 +1,24 @@
+public class Peso200Dispenser implements DispenseChain {
+    private DispenseChain chain;
+
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        this.chain = nextChain;
+    }
+
+
+    @Override
+    public void dispense(Currency cur) {
+        if (cur.getAmount() >= 200) {
+            int num = cur.getAmount() / 200;
+            int remainder = cur.getAmount() % 200;
+            System.out.println("Dispensing " + num + " 200 bills");
+            if (remainder != 0) {
+                this.chain.dispense(new Currency(remainder));
+            }
+        } else {
+            this.chain.dispense(cur);
+        }
+    }
+}
