4297b6691aff83cd5ee036c0471c8e177da869dc
tomacruzjosh <tomacruzj415@gmail.com>
2026-02-24 08:46:59 +0800

    Update BPI_Atm.java
    Modified the program to be compatible with at least 20 peso bills.

--- BPI_Atm.java
+++ BPI_Atm.java
@@ -2,8 +2,8 @@ public class BPI_Atm {
     public static void main(String[] args) {
         ATMDispenseChain atmDispenser = new ATMDispenseChain();
         int amount = 2800; // Amount to be dispensed
-        if (amount % 100 != 0) {
-            System.out.println("Amount should be in multiples of 100s.");
+        if (amount % 20 != 0) {
+            System.out.println("Amount should be in multiples of 20s.");
             
         } else
         atmDispenser.dispense(new Currency(amount));
