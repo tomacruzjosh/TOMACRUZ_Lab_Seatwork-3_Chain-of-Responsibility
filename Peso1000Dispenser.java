49031a810f0869f511bc652e383e38419590b9b6
tomacruzjosh <tomacruzj415@gmail.com>
2026-02-24 08:42:35 +0800

    Create Peso1000Dispenser.java

---
+++ Peso1000Dispenser.java
@@ -0,0 +1,24 @@
+public class Peso1000Dispenser implements DispenseChain {
+    private DispenseChain chain;
+
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        this.chain = nextChain;
+    }
+
+
+    @Override
+    public void dispense(Currency cur) {
+        if (cur.getAmount() >= 1000) {
+            int num = cur.getAmount() / 1000;
+            int remainder = cur.getAmount() % 1000; //800
+            System.out.println("Dispensing " + num + " 1000 bills");
+            if (remainder != 0) {
+                this.chain.dispense(new Currency(remainder));
+            }
+        } else {
+            this.chain.dispense(cur);
+        }
+    }
+}
